a7b6af2d136e3d2ca1fd0d442d6636c8c43e4a3a
Michael Eliachevitch <meliache@uni-bonn.de>
2020-03-31 22:48:11 +0200

    Revert "Add generated static sphinx site files for github pages"
    This reverts commit a1d9ea43bdf86fd9496def2427d6b0c55aa863a2.

--- b2luigi/.dir-locals.el
+++
@@ -1,8 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((nil . ((ssh-deploy-root-local . "/home/michael/projects/b2luigi/")
-         (ssh-deploy-root-remote . "/ssh:kek:/home/belle2/meliache/projects/b2luigi/")
-         (pyvenv-workon . "analysistools")))
- (python-mode . ((flycheck-disabled-checkers . (python-mypy))
-                 (flycheck-checker . python-flake8))))
